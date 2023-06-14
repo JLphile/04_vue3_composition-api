9f2449eee569d2c7f2ed774adabc57a48edac362
Jlphile <lkyz001@outlook.com>
2023-06-14 10:25:47 +0800

    11-setup语法糖

---
+++ src/11_setup语法糖/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <ShowInfo name="Jack" :age="22" @info-btn-click="onInfoBtnClick" ref="showInfoRef"></ShowInfo>
+  <ShowInfo></ShowInfo>
+</template>
+
+<script setup>
+import ShowInfo from './ShowInfo.vue'
+import{ref,onMounted} from 'vue'
+
+function onInfoBtnClick(payload) {
+  console.log('监听到showInfo内部的点击:', payload)
+}
+
+// 获取组件实例
+const showInfoRef = ref()
+onMounted(() => {
+  showInfoRef.value.foo()
+}
+)
+
+</script>
+
+<style scoped></style>
---
+++ src/11_setup语法糖/ShowInfo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>ShowInfo:{{ name }}-{{ age }}</div>
+  <button @click="showInfoBtnClick">change info</button>
+</template>
+
+<script setup>
+import {defineExpose} from 'vue'
+const props = defineProps({
+  name: {
+    type: String,
+    default: 'Rose',
+  },
+  age: {
+    type: Number,
+    default: 0,
+  },
+})
+
+const emits = defineEmits()
+function showInfoBtnClick() {
+  emits('infoBtnClick', 'showInfo内部发生了点击')
+}
+
+function foo() {
+  console.log('ShowInfo foo函数被调用了')
+}
+
+defineExpose({
+  foo,
+})
+</script>
+
+<style scoped></style>
